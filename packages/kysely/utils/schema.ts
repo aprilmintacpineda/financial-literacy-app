1f745758b62bf84af22114957e97d66ddb49c57d
April Mintac Pineda <aprilmintacpineda@gmail.com>
2025-07-06 11:58:14 +0800

    deleted_at column should be nullable;

--- packages/kysely/utils/schema.ts
+++ packages/kysely/utils/schema.ts
@@ -1,9 +1,9 @@
 import { type Kysely } from 'kysely';
 
-export function createBaseTable (db: Kysely<any>, name: string) {
+export function createBaseTable(db: Kysely<any>, name: string) {
   return db.schema
     .createTable(name)
     .addColumn('createdAt', 'timestamp', col => col.notNull())
     .addColumn('updatedAt', 'timestamp', col => col.notNull())
-    .addColumn('deletedAt', 'timestamp', col => col.notNull());
+    .addColumn('deletedAt', 'timestamp');
 }

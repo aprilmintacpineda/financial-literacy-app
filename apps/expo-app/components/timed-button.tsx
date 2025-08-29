2205f376608d58b753e01c78edb0e22a453417d1
April Mintac Pineda <aprilmintacpineda@gmail.com>
2025-08-29 22:50:22 +0800

    feat: button component that is automatically disabled based on count down

---
+++ apps/expo-app/components/timed-button.tsx
@@ -0,0 +1,50 @@
+import {
+  Duration,
+  Instant,
+  LocalDateTime,
+  ZoneId,
+} from '@js-joda/core';
+import { type ComponentProps, useEffect, useState } from 'react';
+import Button from './button';
+
+function secondsLeft (targetDate: Date) {
+  const duration = Duration.between(
+    LocalDateTime.now(),
+    LocalDateTime.ofInstant(
+      Instant.ofEpochMilli(targetDate.getTime()),
+      ZoneId.systemDefault(),
+    ),
+  );
+
+  return duration.seconds();
+}
+
+export default function TimedButton ({
+  toDate,
+  label,
+  isDisabled,
+  ...btnProps
+}: {
+  toDate: Date;
+  label: string;
+} & Omit<ComponentProps<typeof Button>, 'label'>) {
+  const [count, setCount] = useState(() => secondsLeft(toDate));
+
+  useEffect(() => {
+    setCount(secondsLeft(toDate));
+
+    const timer = setInterval(() => {
+      setCount(secondsLeft(toDate));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [toDate]);
+
+  return (
+    <Button
+      {...btnProps}
+      label={`${count > 0 ? `${label} (${count})` : label}`}
+      isDisabled={count > 0 || isDisabled}
+    />
+  );
+}

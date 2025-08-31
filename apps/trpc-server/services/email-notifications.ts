dae1b018c8d59f762aef944cf000e61c03fb66d0
April Mintac Pineda <aprilmintacpineda@gmail.com>
2025-08-31 18:03:50 +0800

    feat: small refactor

--- apps/trpc-server/services/email-notifications.ts
+++ apps/trpc-server/services/email-notifications.ts
@@ -1,6 +1,13 @@
 import { sendEmail } from '../utils/s3';
 
-export async function sendWelcomeEmail({
+function composeBody (message: string[]) {
+  return [
+    ...message,
+    '<p>Best regards,<br/>The Cash Tracker App Team</p>',
+  ].join('');
+}
+
+export async function sendWelcomeEmail ({
   emailVerificationCode,
   to,
   name,
@@ -13,19 +20,18 @@ export async function sendWelcomeEmail({
     await sendEmail({
       to,
       subject: 'Welcome to Cash Tracker App',
-      body: [
+      body: composeBody([
         `<p>Hi ${name},</p>`,
         `<p>Thank you for signing up for Cash Tracker App! We're excited to have you on board. Please use the code <strong>${emailVerificationCode}</strong> to verify your email.</p>`,
         '<p>Please note that this code will expire in 15 minutes.</p>',
-        '<p>Best regards,<br/>The Cash Tracker App Team</p>',
-      ].join('\n'),
+      ]),
     });
   } catch (error) {
     console.error('Error sending welcome email:', error);
   }
 }
 
-export async function resendEmailVerificationCode({
+export async function resendEmailVerificationCode ({
   emailVerificationCode,
   to,
   name,
@@ -38,12 +44,11 @@ export async function resendEmailVerificationCode({
     await sendEmail({
       to,
       subject: 'Resend Email Verification Code',
-      body: [
+      body: composeBody([
         `<p>Hi ${name},</p>`,
         `<p>Looks like you missed your previous email verification code! Don't worry, we got your back, here's another code that you can use to verify your email: <strong>${emailVerificationCode}</strong></p>`,
         '<p>Please note that this code will expire in 15 minutes.</p>',
-        '<p>Best regards,<br/>The Cash Tracker App Team</p>',
-      ].join('\n'),
+      ]),
     });
   } catch (error) {
     console.error('Error sending email verification code:', error);

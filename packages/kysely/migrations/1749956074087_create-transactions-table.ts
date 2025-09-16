ead3afc83de27d4ecf83bf72fbe2c4e8b2ca97b6
April Mintac Pineda <aprilmintacpineda@gmail.com>
2025-09-16 22:57:23 +0800

    feat: transactions table will handle transfer and other transaction types

--- packages/kysely/migrations/1749956074087_create-transactions-table.ts
+++ packages/kysely/migrations/1749956074087_create-transactions-table.ts
@@ -3,16 +3,18 @@ import { createBaseTable } from '../utils/schema';
 import { cuid } from '../utils/types';
 
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
-export async function up (db: Kysely<any>): Promise<void> {
+export async function up(db: Kysely<any>): Promise<void> {
   await createBaseTable(db, 'transactions')
     .addColumn('id', cuid, col => col.primaryKey().notNull())
     .addColumn('organizationId', cuid, col => col.notNull())
     .addColumn('walletId', cuid, col => col.notNull())
-    .addColumn('categoryId', cuid, col => col.notNull())
-    .addColumn('description', 'varchar(255)', col => col.notNull())
+    .addColumn('categoryId', cuid)
+    .addColumn('description', 'varchar(255)')
     .addColumn('amount', 'bigint', col => col.notNull())
     .addColumn('transactionDate', 'timestamp', col => col.notNull())
-    .addColumn('transactionType', 'varchar(7)', col => col.notNull())
+    .addColumn('transactionType', 'varchar(9)', col => col.notNull())
+    .addColumn('fromWalletId', cuid)
+    .addColumn('exchangeRate', 'bigint')
     .addColumn('currency', 'varchar(3)', col => col.notNull())
     .execute();
 
@@ -35,6 +37,12 @@ export async function up (db: Kysely<any>): Promise<void> {
     .execute();
 
   await db.schema
+    .createIndex('fromWalletId_idx')
+    .on('transactions')
+    .column('fromWalletId')
+    .execute();
+
+  await db.schema
     .createIndex('transactionDate_idx')
     .on('transactions')
     .column('transactionDate')
@@ -54,6 +62,6 @@ export async function up (db: Kysely<any>): Promise<void> {
 }
 
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
-export async function down (db: Kysely<any>): Promise<void> {
+export async function down(db: Kysely<any>): Promise<void> {
   await db.schema.dropTable('transactions').execute();
 }

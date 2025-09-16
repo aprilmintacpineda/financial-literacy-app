d1331a30142ad56e0d0a18345eb91ec1b10f391e
April Mintac Pineda <aprilmintacpineda@gmail.com>
2025-09-16 23:00:32 +0800

    feat: added procedure outputs type

--- apps/expo-app/utils/trpc.ts
+++ apps/expo-app/utils/trpc.ts
@@ -3,6 +3,8 @@ import {
   createTRPCReact,
   type TRPCClientError,
 } from '@trpc/react-query';
+import { type inferRouterOutputs } from '@trpc/server';
 
 export const trpc = createTRPCReact<tAppRouter>();
 export type tTRPCClientError = TRPCClientError<tAppRouter>;
+export type TRPCProcedureOutputs = inferRouterOutputs<tAppRouter>;

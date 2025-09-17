ecfa2008a5d8a598261873fef9f3587daad5bae4
April Mintac Pineda <aprilmintacpineda@gmail.com>
2025-09-17 20:57:02 +0800

    fix: clearer email validation error message

--- packages/data-transfer-objects/dtos/sign-in.ts
+++ packages/data-transfer-objects/dtos/sign-in.ts
@@ -4,7 +4,7 @@ export const signInDto = z.object({
   email: z
     .string({ invalid_type_error: 'Email must be a string' })
     .nonempty('Please enter your email')
-    .email('Please enter your email'),
+    .email('Please enter a valid email address'),
   password: z
     .string({ invalid_type_error: 'Password must be a string' })
     .nonempty('Please enter your password'),
